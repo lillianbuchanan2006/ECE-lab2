4d532a64036d5b3bbda1c97e4b114d075287d417
lillianbuchanan2006 <lillianbuchanan2006@users.noreply.github.com>
2026-03-04 17:52:55 -0700

    testbench

--- src/sevenseg_decoder_tb.vhd
+++ src/sevenseg_decoder_tb.vhd
@@ -145,14 +145,4 @@ begin
 end test_bench;
 ---------------------------------------------^ copied from lab 1 
 
-library IEEE;
-use IEEE.STD_LOGIC_1164.ALL;
 
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx leaf cells in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
